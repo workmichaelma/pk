866084e79726b20bf89958bc7fe44b408b7998e2
Michael <work.michael.ma@gmail.com>
2022-09-02 04:39:25 +0800

    Update port and cronjob

--- backend/config/cron-tasks.js
+++ backend/config/cron-tasks.js
@@ -4,14 +4,104 @@ module.exports = {
    * Every monday at 1am.
    */
 
-  "* 0 * * *": ({ strapi }) => {
-    strapi.api["fetch-products"].services["cron-products"].cronProducts(
-      "04000000"
-    );
+  "* 2 * * *": ({ strapi }) => {
+    console.log(`Cron - 04010000`, new Date());
+    try {
+      strapi.api["fetch-products"].services["cron-products"].cronProducts(
+        "04010000"
+      );
+    } catch (err) {
+      console.log(err);
+    }
   },
-  "5 0 * * *": ({ strapi }) => {
-    strapi.api["fetch-products"].services["cron-products"].cronProducts(
-      "04020000"
-    );
+  "5 2 * * *": ({ strapi }) => {
+    console.log(`Cron - 04020000`, new Date());
+    try {
+      strapi.api["fetch-products"].services["cron-products"].cronProducts(
+        "04020000"
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  "10 2 * * *": ({ strapi }) => {
+    console.log(`Cron - 04030000`, new Date());
+    try {
+      strapi.api["fetch-products"].services["cron-products"].cronProducts(
+        "04030000"
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  "15 2 * * *": ({ strapi }) => {
+    console.log(`Cron - 04040000`, new Date());
+    try {
+      strapi.api["fetch-products"].services["cron-products"].cronProducts(
+        "04040000"
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  "20 2 * * *": ({ strapi }) => {
+    console.log(`Cron - 04050000`, new Date());
+    try {
+      strapi.api["fetch-products"].services["cron-products"].cronProducts(
+        "04050000"
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  "25 2 * * *": ({ strapi }) => {
+    console.log(`Cron - 04060000`, new Date());
+    try {
+      strapi.api["fetch-products"].services["cron-products"].cronProducts(
+        "04060000"
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  "30 2 * * *": ({ strapi }) => {
+    console.log(`Cron - 04070000`, new Date());
+    try {
+      strapi.api["fetch-products"].services["cron-products"].cronProducts(
+        "04070000"
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  "35 2 * * *": ({ strapi }) => {
+    console.log(`Cron - 04080000`, new Date());
+    try {
+      strapi.api["fetch-products"].services["cron-products"].cronProducts(
+        "04080000"
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  "40 2 * * *": ({ strapi }) => {
+    console.log(`Cron - 04090000`, new Date());
+    try {
+      strapi.api["fetch-products"].services["cron-products"].cronProducts(
+        "04090000"
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  "45 2 * * *": ({ strapi }) => {
+    console.log(`Cron - 04100000`, new Date());
+    try {
+      strapi.api["fetch-products"].services["cron-products"].cronProducts(
+        "04100000"
+      );
+    } catch (err) {
+      console.log(err);
+    }
   },
 };
--- backend/config/server.js
+++ backend/config/server.js
@@ -2,7 +2,7 @@ const cronTasks = require("./cron-tasks");
 
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
+  port: env.int("PORT", 1000),
   app: {
     keys: env.array("APP_KEYS"),
   },
