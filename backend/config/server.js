46b5400fce06a3c8875ee94faa1d3aef2291bf2c
Michael <work.michael.ma@gmail.com>
2022-09-05 03:37:20 +0800

    Revert to 1337

--- backend/config/server.js
+++ backend/config/server.js
@@ -2,7 +2,7 @@ const cronTasks = require("./cron-tasks");
 
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1000),
+  port: env.int("PORT", 1337),
   app: {
     keys: env.array("APP_KEYS"),
   },
